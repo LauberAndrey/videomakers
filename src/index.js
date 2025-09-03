827d331848c75a1d5cdc3935ec70aebc6535ffd5
QuinnV1 <lenova172004@mail.ru>
2025-09-04 01:02:31 +0300

    изменил пути видосов

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/reset.css';
-import './styles/fonts.css';
+import './styles/fonts.css'
 import './styles/globals.css';
 import App from './App';
 
--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -66,7 +66,7 @@ const Header = ({ scrollRef }) => {
 							onEnded={(e) => e.target.currentTime = 0}
                         >
                             
-							<source src="/video/Showreel Short Loop.mp4" type="video/webm" />
+							<source src="/video/showreel-short-loop.mp4" type="video/mp4" />
                         </video>
                         
                         <div className='header_up'>
